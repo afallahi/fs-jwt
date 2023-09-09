20a2e9c15837f433c463fcfdb79204cc68c4beca
Afshin <fallahi_a@yahoo.com>
2023-09-09 19:13:54 -0400

    added Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { createDocument } from './swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  SwaggerModule.setup('api', app, createDocument(app));
   await app.listen(3000);
 }
 bootstrap();
