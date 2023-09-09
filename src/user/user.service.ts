8523b0a1d6324b860d1112f539a50d5f867f4984
Afshin <fallahi_a@yahoo.com>
2023-09-09 16:49:14 -0400

    add user module

---
+++ src/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+    
+    getHello(): string {
+        return 'Hello World!';
+      }    
+
+    postHello(): string {
+        return 'Hello World!';
+    }    
+
+}
